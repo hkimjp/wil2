dbceb92039311438b1859f41146bde1243d0c6e3
Hiroshi Kimura <hkim@hkim.jp>
2025-09-05 16:16:50 +0900

    under construction

--- src/hkimjp/wil2/my.clj
+++ src/hkimjp/wil2/my.clj
@@ -5,12 +5,31 @@
    [hkimjp.wil2.view :refer [page]]
    [taoensso.telemere :as t]))
 
+(def sent '[:find ?e ?pt
+            :in $ ?login
+            :where
+            [?e :wil2 "point"]
+            [?e :login ?login]
+            [?e :pt ?pt]])
+
+(def sent-count '[:find (count ?e)
+                  :in $ ?login ?pt
+                  :where
+                  [?e :wil2 "point"]
+                  [?e :login ?login]
+                  [?e :pt ?pt]])
+
+; (ds/qq sent "hkimura")
+; (ds/qq sent-count "hkimura" 1)
+; (ds/qq sent-count "hkimura" 2)
+; (ds/qq sent-count "hkimura" -1)
+
 (defn my [request]
-  (t/log! :debug "my")
-  (page
-   [:div
-    [:div.text-2xl (user  request) "'s points"]
-    [:div
-     [:div.font-bold "送信ポイント"]
-     [:p "⬆️　➡️　⬇️"]
-     [:div.font-bold "受信ポイント"]]]))
+  (let [user (user request)]
+    (t/log! :info (str "my " user))
+    (page
+     [:div
+      [:div.text-2xl (user  request) "'s points"]
+      [:div
+       [:div.font-bold "Points by Sendings"]
+       [:div.font-bold "Points by Receivings"]]])))
