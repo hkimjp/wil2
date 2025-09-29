7970bc4d2833db7671c88614be30f45f2ef513ea
hkimjp <hkim@hkim.jp>
2025-09-29 20:31:57 +0900

    docker container. worked on eq.local

--- src/hkimjp/wil2/view.clj
+++ src/hkimjp/wil2/view.clj
@@ -4,7 +4,7 @@
    [ring.util.response :as resp]
    [taoensso.telemere :as t]))
 
-(def version "0.3.10")
+(def version "0.3.11")
 
 (def ^:private menu "text-xl font-medium text-white px-1 hover:bg-green-700")
 
