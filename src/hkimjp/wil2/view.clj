a2a5c2a2df4e92e9c1bf880d4eef860b4c83a897
Hiroshi Kimura <hkim@hkim.jp>
2025-09-30 09:47:50 +0900

    used `(seq #{})` to judge emptyness of #{}

--- src/hkimjp/wil2/view.clj
+++ src/hkimjp/wil2/view.clj
@@ -4,7 +4,7 @@
    [ring.util.response :as resp]
    [taoensso.telemere :as t]))
 
-(def version "0.3.11")
+(def version "0.3.12-SNAPSHOT")
 
 (def ^:private menu "text-xl font-medium text-white px-1 hover:bg-green-700")
 
