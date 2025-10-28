7eb4a6241c3a5a5939bf2879f0dc98a790798556
Hiroshi Kimura <hkim@hkim.jp>
2025-10-28 15:20:14 +0900

    added `/admin/delete` which will update eid entry as `:wil2 "delete"`

--- src/hkimjp/wil2/view.clj
+++ src/hkimjp/wil2/view.clj
@@ -4,7 +4,7 @@
    [ring.util.response :as resp]
    [taoensso.telemere :as t]))
 
-(def version "0.3.20")
+(def version "0.3.21")
 
 (def ^:private menu "text-xl font-medium text-white px-1 hover:bg-green-700")
 
